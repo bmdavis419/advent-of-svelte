b90a4caf68861b855f918a29bfb5ffa6936c9f4e
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-12-01 10:46:39 -0500

    oranized day 1

--- day-1/src/routes/+page.server.ts
+++ day-1/src/routes/+page.server.ts
@@ -3,29 +3,27 @@ type ExtDataPayload = {
 	tally: number;
 }[];
 
-export const load = async ({ fetch }) => {
-	const res = await fetch('https://advent.sveltesociety.dev/data/2023/day-one.json');
-
-	const data = (await res.json()) as ExtDataPayload;
-
+// O(n^3)
+const algOne = (data: ExtDataPayload) => {
 	// split into a naughty and nice array, sorted
 	const naughty: ExtDataPayload = [];
 	const nice: ExtDataPayload = [];
 
-	// this is O(n^2) I think...
+	// this is O(n^3)
 	data.forEach((item) => {
 		if (item.tally <= 0) {
-			insertInOrder(naughty, item); // O(n)
+			insertInOrder(naughty, item); // O(n^2)
 		} else {
-			insertInOrder(nice, item); // O(n)
+			insertInOrder(nice, item); // O(n^2)
 		}
 	});
 
-	// another way you could do this is just run the .sort() method (I have to guess its O(log(n)))
-	// the problem is we have to split it into an even and odd...
-	// another idea:
+	return { naughty, nice };
+};
 
-	// O(log(n))
+// O(nlog(n) + O(n) + O(n) + O(n) = O(nlog(n)))
+const algTwo = (data: ExtDataPayload) => {
+	// O(nlog(n))
 	const sorted = data.sort((a, b) => a.tally - b.tally);
 
 	let idx = 0;
@@ -38,31 +36,45 @@ export const load = async ({ fetch }) => {
 		idx++;
 	}
 
-	const naughty2 = sorted.slice(0, idx);
-	const nice2 = sorted.slice(idx, sorted.length);
+	// O(n) (there is some probabilistic analysis that could be done here, but nah)
+	const naughty = sorted.slice(0, idx);
+
+	// O(n)
+	const nice = sorted.slice(idx, sorted.length);
+
+	// O(n)
+	return { naughty, nice: nice.reverse() };
+};
+
+export const load = async ({ fetch }) => {
+	const res = await fetch('https://advent.sveltesociety.dev/data/2023/day-one.json');
+
+	const data = (await res.json()) as ExtDataPayload;
+
+	const alg1Res = algOne(data);
+	const alg2Res = algTwo(data);
 
 	return {
-		naughty,
-		nice,
-		naughty2,
-		// idk if there is a way to get rid of this reverse call (I think its log(n))
-		nice2: nice2.reverse()
+		naughty: alg1Res.naughty,
+		nice: alg1Res.nice,
+		naughty2: alg2Res.naughty,
+		nice2: alg2Res.nice
 	};
 };
 
 // pretty sure that this is unhinged but its the insert sort I came up with at 2 AM
-// I think this is O(n)
+// I think this is O(n^2)
 const insertInOrder = (a: ExtDataPayload, item: { name: string; tally: number }) => {
 	let idx = 0;
 	while (idx < a.length) {
 		if (item.tally > 0) {
 			if (a[idx].tally <= item.tally) {
-				a.splice(idx, 0, item);
+				a.splice(idx, 0, item); // O(n)
 				return;
 			}
 		} else {
 			if (a[idx].tally > item.tally) {
-				a.splice(idx, 0, item);
+				a.splice(idx, 0, item); // O(n)
 				return;
 			}
 		}
