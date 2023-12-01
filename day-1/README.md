afef68dcb3274403a7106e574411c1f450a53cca
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-12-01 11:10:48 -0500

    UI

--- day-1/README.md
+++ day-1/README.md
@@ -5,3 +5,5 @@ I am not quite done with this one yet, I spent some time messing around with the
 Its been a year and a half since I've done any algorithms so its a bit of a mess lol.
 
 IMPORTANT FILE: ```src/routes/+page.server.ts```
+
+https://advent.sveltesociety.dev/#challenges
--- day-1/src/routes/+page.svelte
+++ day-1/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import * as Table from '$lib/components/ui/table';
+
 	const { data } = $props();
 
 	// will add later
@@ -6,18 +8,44 @@
 	// const nice = $state(data.nice);
 </script>
 
-<div class="flex flex-row justify-between">
-	<div class="flex flex-col items-center w-1/2">
-		<h2>NAUGHTY</h2>
-		{#each data.naughty as bad}
-			<div>{bad.name}: {bad.tally}</div>
-		{/each}
+<div class="flex flex-row">
+	<div class="w-1/2">
+		<Table.Root class="w-[400px] mx-auto">
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="w-[100px]">Name</Table.Head>
+
+					<Table.Head class="text-right">Tally</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				{#each data.nice as good}
+					<Table.Row>
+						<Table.Cell class="font-medium">{good.name}</Table.Cell>
+						<Table.Cell class="text-right">{good.tally}</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
 	</div>
 
-	<div class="flex flex-col items-center w-1/2">
-		<h2>NICE</h2>
-		{#each data.nice as good}
-			<div>{good.name}: {good.tally}</div>
-		{/each}
+	<div class="w-1/2">
+		<Table.Root class="w-[400px] mx-auto">
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="w-[100px]">Name</Table.Head>
+
+					<Table.Head class="text-right">Tally</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				{#each data.naughty as bad}
+					<Table.Row>
+						<Table.Cell class="font-medium">{bad.name}</Table.Cell>
+						<Table.Cell class="text-right">{bad.tally}</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
 	</div>
 </div>
